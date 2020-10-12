ec2f287a84c041243afebc8c72eefee4eff04830
Pawel Szopa <expiren@gmail.com>
2020-10-12 19:25:28 +0200

    full simple reservation system

--- app.py
+++ app.py
@@ -45,12 +45,47 @@ class Flight:
         self.seating_plan[row][letter] = passenger
 
     def relocate_passenger(self, seat_from, seat_to):
+
+        row_from, letter_from = self._parse_seat(seat_from)
+        if self.seating_plan[row_from][letter_from] is None:
+            raise ValueError(f'Seat is not occupied like {seat_from}')
         row_to, letter_to = self._parse_seat(seat_to)
         if self.seating_plan[row_to][letter_to] is not None:
             raise ValueError(f"Seat is already taken {seat_to}")
-        row_from, letter_from = self._parse_seat(seat_from)
+
         self.seating_plan[row_to][letter_to] = self.seating_plan[row_from][letter_from]
         self.seating_plan[row_from][letter_from] = None
+
+    def num_empty_seats(self):
+        return sum(sum(1 for seat in row.values() if seat is None) for row in self.seating_plan
+                   if row is not None)
+
+    def print_cards(self, printer):
+        # passeengers = self.get_passengers()
+        for passenger, seat in self.get_passengers():
+            printer(passenger, seat, self.get_airplane_model(), self.flight_number)
+
+    def get_passengers(self):
+        rows, letters = self.airplane.seating_plan()
+        for row in rows:
+            for letter in letters:
+                passenger = self.seating_plan[row][letter]
+                if passenger is not None:
+                    yield passenger, f'{row}{letter}'
+
+
+''' generatory  get passenger
+
+        rows, letters  = self.seating_plan()
+        for row in rows:
+            for letter in letters:
+                passenger = self.seating_plan[row][letter]
+                if passenger is not None:
+                    passenger_data= passenger,f'{row}{letter}'
+                    passengers.append(passenger_data)
+
+        return passengers
+'''
 
 
 class Airplane:
@@ -79,7 +114,19 @@ class AirbusA370(Airplane):
         return range(1, 40), 'ABCDEGHJK'
 
 
-f = Flight('BA123', Boeing737())
+def card_printer(name, seat, plane_model, flight_number):
+    text = f'| Pasażer: {name}, Siedzenie: {seat}, Model: {plane_model}, FN: {flight_number} |'
+    frame='+'+((len(text)-2)*'-')+'+'
+    # frame= f"+{'-' *(len(text)-2)}+"
+    empty_frame = f"|{' '*(len(text)-2)}|"
+    border =[frame,empty_frame,text,empty_frame,frame]
+    border_text= '\n'.join(border)
+    print(border_text)
+
+
+
+b = Boeing737()
+f = Flight('BA123', b)
 # print(f.get_airlines())
 # print(f.get_number())
 # print(f.get_airplane_model())
@@ -88,9 +135,11 @@ f = Flight('BA123', Boeing737())
 
 # a= AirbusA370()
 # print(a.num_seats())
-f.allocate_passenger('Lech K', '1A')
-f.allocate_passenger('Jaroslaw K', '2B')
+f.allocate_passenger('Lech K', '1B')
+f.allocate_passenger('Jaroslaw K', '1A')
 f.allocate_passenger('Pawel K', '3B')
 pprint(f.seating_plan)
-f.relocate_passenger('2B', "14C")
-pprint(f.seating_plan)
+f.relocate_passenger('1B', "5B")
+# print(f.seating_plan)
+# print(f.num_empty_seats())
+f.print_cards(card_printer)  # higher order function first class function
